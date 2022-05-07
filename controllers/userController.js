6e064d002d13a47bc01a8cf3c5c2d35744ee5937
GittinIt6 <mjeluso@gmail.com>
2022-05-06 22:04:34 -0600

    user api manipulation

--- controllers/userController.js
+++ controllers/userController.js
@@ -58,4 +58,57 @@ module.exports = {
           res.status(500).json(err);
         });
     },
+
+    //update user by ID with JSON username or email
+    updateUser(req, res) {
+      console.log(`Updating User ${req.body.userName}`);
+      User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { strict: false, runValidators: true, new: true }
+      )
+        .then((user) =>
+          !user
+            ? res
+                .status(404)
+                .json({ message: 'No user found with that ID' })
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+
+      // Add a friend to a user
+  addFriend(req, res) {
+    console.log('You are adding a friend');
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { strict: false, runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  //delete friend from user
+  deleteFriend(req, res) {
+    console.log('You are removing a friend');
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
